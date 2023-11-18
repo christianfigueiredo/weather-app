2a04b11ce674b75273582ddcb53e1b9c97fa79ec
Christian Brandão Figueiredo <christian.figueiredo@vixteam.com.br>
2023-11-18 19:23:13 -0300

    add interface para leitura dos dados da api

--- src/app/modules/weather/page/weather-home/weather-home.component.ts
+++ src/app/modules/weather/page/weather-home/weather-home.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { WeatherDatas } from 'src/app/models/interfaces/weatherDatas';
 
 @Component({
   selector: 'app-weather-home',
   templateUrl: './weather-home.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
-export class WeatherHomeComponent {
+export class WeatherHomeComponent implements OnInit{
 
-}
+  initialCity: string = 'Rio das Ostras';
+  weatherDatas!: WeatherDatas;
+  
+  constructor(private weatherService: WeatherService) { }
+  ngOnInit(): void {
+    this.getWeatherDatas(this.initialCity);
+  }
+
+  getWeatherDatas(cityName: string):void{
+    this.weatherService.getWeatherDatas(cityName)
+    .subscribe({
+      next: (response) => {
+        response && (this.weatherDatas = response);
+        console.log("A temperatura maxima e: ",this.weatherDatas.main.temp_max);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    })
+
+  }
+    
+    
+  }
+
+
